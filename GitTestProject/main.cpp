542c23203dae8dde599de82ad6de226b3409a065
JiangNan <jn200319@outlook.com>
2022-07-12 19:17:26 +0800

    Initial Commit

---
+++ GitTestProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitTestProject
+//
+//  Created by JiangNan on 2022/7/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
